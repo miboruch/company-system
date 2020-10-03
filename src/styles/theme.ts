86d62e39698c8b57c61e5dd36f359eb5c65eb29d
Michał Boruch <miboruch@gmail.com>
2020-10-03 16:34:56 +0200

    Added theme file for styled components provider

---
+++ src/styles/theme.ts
@@ -0,0 +1,42 @@
+interface Breakpoints {
+  [mobile: string]: number;
+  tabletS: number;
+  tablet: number;
+  standard: number;
+  desktop: number;
+}
+
+export const breakpoints: Breakpoints = {
+  mobile: 340,
+  tabletS: 512,
+  tablet: 710,
+  standard: 1024,
+  desktop: 1440
+};
+
+export const theme = {
+  mq: Object.keys(breakpoints).reduce((acc, breakpoint) => {
+    acc[breakpoint] = `@media all and (min-width: ${breakpoints[breakpoint]}px)`;
+
+    return acc;
+  }, {} as { [key: string]: string }),
+  font: {
+    family: {
+      montserrat: 'Test'
+    }
+  },
+  colors: {
+    background: '#f6f6f1',
+    infoButton: 'rgba(0,0,0,0.8)',
+    // infoButton: '#aeaeae',
+    green: '#429f33',
+    darkGreen: '#3e6406',
+    red: '#9f3333',
+    footerLinks: '#b6b6b6',
+    landingBackground: '#0E0E0E',
+    landingGray: '#5A5A5A',
+    white: '#f5f5f5',
+    qrBackground: '#50D889',
+    blue: '#1573E3'
+  }
+};
