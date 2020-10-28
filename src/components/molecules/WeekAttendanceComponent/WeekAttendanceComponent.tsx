20a64f67762276b92d43af3ee468c5b077c14bfc
Michał Boruch <miboruch@gmail.com>
2020-10-28 16:38:15 +0100

    Added week attendance component

--- src/components/molecules/WeekAttendanceComponent/WeekAttendanceComponent.tsx
+++ src/components/molecules/WeekAttendanceComponent/WeekAttendanceComponent.tsx
@@ -1,30 +1,72 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import styled, { css } from 'styled-components';
-import { ClientInterface, WeekAttendance } from '../../../types/modelsTypes';
-import { EmptyIcon, NotCheckedIcon, CheckedIcon } from '../../../styles/iconStyles';
-import { AppState } from '../../../reducers/rootReducer';
-import { ThunkDispatch } from 'redux-thunk';
-import { AppTypes } from '../../../types/actionTypes/appActionTypes';
 import { bindActionCreators } from 'redux';
-import { getCompanyClients, selectClient, setClientInfoOpen } from '../../../actions/clientActions';
+import { ThunkDispatch } from 'redux-thunk';
+import styled, { css } from 'styled-components';
+import { WeekAttendance } from '../../../types/modelsTypes';
+import { CheckedIcon, EmptyIcon, NotCheckedIcon } from '../../../styles/iconStyles';
+import { compareDates } from '../../../utils/functions';
+import ArrowButton from '../../atoms/ArrowButton/ArrowButton';
+import { Direction } from '../../../types/globalTypes';
 import { getWeekAttendance } from '../../../actions/attendanceActions';
+import { AppTypes } from '../../../types/actionTypes/appActionTypes';
+
+const MainWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 5rem;
+`;
+
+const Header = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const StyledWrapper = styled.div`
   width: 100%;
-  height: 100px;
   display: grid;
-  grid-template-columns: repeat(7, 1fr);
+  grid-template-rows: repeat(7, 1fr);
+  grid-template-columns: auto;
   background-color: ${({ theme }) => theme.colors.impactGray};
   grid-gap: 1px;
+
+  ${({ theme }) => theme.mq.hdReady} {
+    height: 100px;
+    grid-template-columns: repeat(7, 1fr);
+    grid-template-rows: auto;
+  }
 `;
 
-const SingleAttendanceWrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.white};
+interface SingleAttendanceWrapperInterface {
+  isCurrentDay: boolean;
+}
+
+const SingleAttendanceWrapper = styled.div<SingleAttendanceWrapperInterface>`
+  background-color: ${({ theme }) => theme.colors.contentBackground};
+  border: ${({ isCurrentDay }) => (isCurrentDay ? '1px solid #85BE9B' : 'none')};
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding: 2rem 0;
+  transition: background-color 0.4s ease;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.white};
+  }
+
+  ${({ theme }) => theme.mq.hdReady} {
+    background-color: ${({ theme }) => theme.colors.white};
+    padding: 0;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.contentBackground};
+    }
+  }
 `;
 
 const DateParagraph = styled.p`
@@ -71,18 +113,20 @@ const WeekAttendanceComponent: React.FC<ConnectedProps> = ({ weekAttendance, get
   }, [weekCounter]);
 
   return (
-    <>
-      <p onClick={() => decreaseWeek()}>left</p>
-      <p onClick={() => increaseWeek()}>right</p>
+    <MainWrapper>
+      <Header>
+        <ArrowButton direction={Direction.Left} onClick={() => decreaseWeek()} />
+        <ArrowButton direction={Direction.Right} onClick={() => increaseWeek()} />
+      </Header>
       <StyledWrapper>
         {weekAttendance.map((attendance) => (
-          <SingleAttendanceWrapper>
+          <SingleAttendanceWrapper isCurrentDay={compareDates(new Date(attendance.date), new Date())} onClick={() => console.log(attendance)}>
             <DateParagraph>{new Date(attendance.date).toLocaleDateString()}</DateParagraph>
             {attendance.wasPresent === null ? <StyledEmptyIcon /> : attendance.wasPresent ? <StyledCheckedIcon /> : <StyledNotCheckedIcon />}
           </SingleAttendanceWrapper>
         ))}
       </StyledWrapper>
-    </>
+    </MainWrapper>
   );
 };
 
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,7 @@
 export const isEmpty = (obj: object | undefined): boolean => {
   return obj !== undefined ? Object.keys(obj).length === 0 && obj.constructor === Object : true;
 };
+
+export const compareDates = (firstDate: Date, secondDate: Date): boolean => {
+  return firstDate.getFullYear() === secondDate.getFullYear() && firstDate.getMonth() === secondDate.getMonth() && firstDate.getDay() === secondDate.getDay();
+};
